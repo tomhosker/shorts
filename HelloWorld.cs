fe429b39a986d05f1179ab5f22b29057de50d62c
tomhosker <36990655+tomhosker@users.noreply.github.com>
2019-03-20 19:57:14 +0000

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+/*
+A "Hello, world!" program in C#.
+*/
+
+using System;
+
+namespace HelloWorld
+{
+  class Hello 
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello, world!");
+    }
+  }
+}
